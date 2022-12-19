9777c32a4d4224314109ff7a19c518ee6b0ca63c
Laslold <116965660+Laslold@users.noreply.github.com>
2022-12-19 09:55:12 +0000

    Deploying to gh-pages from @ Laslold/just-do-code@2f2cdfef10c7162cb1a98d05d78c09d557c015f4 🚀

--- index.2ae09ce3.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]");e.addEventListener("click",(function(){var n="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!n),t.classList.toggle("is-open")}))}(),function(){var e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-body]");e.addEventListener("click",(function(){t.classList.toggle("is-fixed")}))}();
-//# sourceMappingURL=index.2ae09ce3.js.map
