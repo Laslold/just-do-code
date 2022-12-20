0fbacdb2b754a5f50a20884f81189cf42c36e8ae
Viacheslav Shylko <116965660+Laslold@users.noreply.github.com>
2022-12-20 12:02:06 +0200

    edit script

--- src/js/modal.js
+++ src/js/modal.js
@@ -3,11 +3,19 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    form: document.querySelector('[alt-form]')
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.form.addEventListener("click", formSubmit);
 
+  function formSubmit(e){
+    e.preventDefault();
+    if(e.target.tagName==='BUTTON'){
+      toggleModal();
+    }
+  }
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
@@ -16,17 +24,18 @@
 
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[js-data-modal-open]"),
-    closeModalBtn: document.querySelector("[js-data-modal-close]"),
+    openModalBtn: document.querySelectorAll("[js-data-modal-open]"),
+    closeModalBtn: document.querySelectorAll("[js-data-modal-close]"),
     modal: document.querySelector("[js-data-modal]"),
   };
 
- 
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.forEach(el=>el.addEventListener("click", toggleModal));
+  refs.closeModalBtn.forEach(el=>el.addEventListener("click", toggleModal));
 
   function toggleModal() {
+    document.querySelector('[aside]').style.display = 'none';
+    document.querySelector('[contact]').style.display = 'block';
+    document.querySelector('[js-send-btn]').innerText='Send';
     refs.modal.classList.toggle("is-hidden");
   }
 })();
@@ -37,6 +46,8 @@ const formSend = document.querySelector('[js-send-form]');
 const btnSend = document.querySelector('[js-send-btn]');
 const asideBlock = document.querySelector('[aside]');
 const contactBlock = document.querySelector('[contact]');
+
+
 
 const modal = document.querySelector('[js-data-modal]')
 
@@ -53,7 +64,7 @@ function sendPost (e){
     asideBlock.style.display = 'none';
     contactBlock.style.display = 'block';
     btnSend.innerText='Send';
-    modal.classList.add("is-hidden")
+    modal.classList.add("is-hidden");
     return;
   }
 }
